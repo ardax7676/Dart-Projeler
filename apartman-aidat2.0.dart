7e5d68a3b7232b42e2a639e95bbcd9c5c2dfafc7
ardax76 <ardacicek2011@gmail.com>
2022-05-05 02:34:01 +0300

    test

--- apartman-aidat2.0.dart
+++ apartman-aidat2.0.dart
@@ -11,7 +11,7 @@ void main(List<String> args) {
   Yonetici.uyeEkle(isimler);
 
   Yonetici yonetici1 = Yonetici("Admin", "12");
-  print(Uye.uyeler);
+  print(Yonetici.uyeler);
   yonetici1.aidatOde();
   yonetici1.aidatEkle(2, 200);
   print(yonetici1.bilgiGoruntule(2));
@@ -19,10 +19,10 @@ void main(List<String> args) {
 }
 
 class Uye {
-  static List<Uye> uyeler = [];
+  
   String isim;
   String daire;
-  double? aidat;
+  double? _aidat;
 
   Uye(this.isim, this.daire);
 
@@ -32,24 +32,27 @@ class Uye {
 
   @override
   String toString() {
-    return "Üye ismi: $isim daire numarası: $daire bu ayki aidatı: ${aidat ?? "mevcut değil"} ";
+    return "Üye ismi: $isim daire numarası: $daire bu ayki aidatı: ${_aidat ?? "mevcut değil"} ";
   }
 }
 
 class Yonetici extends Uye {
+
+  static List<Uye> uyeler = [];
+
   Yonetici(String isim, String daire) : super(isim, daire);
 
   void aidatEkle(int daireNo, double aidat) {
-    Uye.uyeler[daireNo - 1].aidat = aidat;
+    Yonetici.uyeler[daireNo - 1]._aidat = aidat;
   }
 
   Uye bilgiGoruntule(int daireNo) {
-    return Uye.uyeler[daireNo - 1];
+    return Yonetici.uyeler[daireNo - 1];
   }
 
   static void uyeEkle(List<String> isimler) {
     for (int i = 0; i < isimler.length; i++) {
-      Uye.uyeler.add(Uye(isimler[i], (i + 1).toString()));
+      Yonetici.uyeler.add(Uye(isimler[i], (i + 1).toString()));
     }
   }
 }
